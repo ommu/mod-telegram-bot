0be76df4866c17a1b9325321f53d56ce632c1477
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-01-12 15:36:12 +0700

    fixed utility

--- modules/telegrambot/components/system/TelegramBotUtility.php
+++ modules/telegrambot/components/system/TelegramBotUtility.php
@@ -32,73 +32,65 @@ class TelegramBotUtility
 		$urlAPI = 'https://api.telegram.org/bot'.$token;
 		if($type == null)
 			$url = $urlAPI.'/'.$method;
-		else {
+		else
 			$parameters["method"] = $method;
-			if($type == 'json')
-				$url = $urlAPI;
-		}
 		
 		if($type != 'webhook') {
 			file_put_contents('telebot_url.txt', $url);
 			
-			$handle = curl_init($url);
+			$handle = curl_init();
+			curl_setopt($handle, CURLOPT_URL, $url);
 			curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
-			curl_setopt($handle, CURLOPT_TIMEOUT, 60);
-			//curl_setopt($handle, CURLOPT_POST, true);			
-		}
-		
-		if($type == null)
-			curl_setopt($handle, CURLOPT_POSTFIELDS, http_build_query($parameters));
-		else {
-			if($type == 'json') {
+			curl_setopt($handle, CURLOPT_TIMEOUT, 0);	
+			if($type == null) {
+				curl_setopt($handle, CURLOPT_POST, true);		
+				curl_setopt($handle, CURLOPT_POSTFIELDS, http_build_query($parameters));
+			}
+			if($type != null && $type == 'json') {
 				curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parameters));	
 				curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-			} else if($type == 'webhook') {
-				header("Content-Type: application/json");
-				echo json_encode($parameters);
-				return true;
-			}	
-		}
-		
-		if($type != 'webhook')
-			return self::execCurlRequest($handle);
-	}
-	
-	public static function execCurlRequest($handle) 
-	{
-		$response = curl_exec($handle);	
- 
-		if($response === false) {
-			$errno = curl_errno($handle);
-			$error = curl_error($handle);
-			error_log("Curl returned error $errno: $error\n");
-			curl_close($handle);
-			return false;
-		}
-		
-		$http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
-		curl_close($handle);
- 
-		if($http_code >= 500) {
-			// do not want to DDOS server if something goes wrong
-			sleep(10);
-			return false;
+			}			
+			$response = curl_exec($handle);	
+	 
+			if($response === false) {
+				file_put_contents('exec_response_false.txt', $response);
+				$errno = curl_errno($handle);
+				$error = curl_error($handle);
+				error_log("Curl returned error $errno: $error\n");
+				curl_close($handle);
+				return false;
+			} else
+				file_put_contents('exec_response_true.txt', $response);
 			
-		} else if($http_code != 200) {
-			$response = json_decode($response, true);
-			error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
-			if($http_code == 401)
-				throw new Exception('Invalid access token provided');
-			return false;
+			$http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+			curl_close($handle);
+	 
+			if($http_code >= 500) {
+				// do not want to DDOS server if something goes wrong
+				sleep(10);
+				return false;
+				
+			} else if($http_code != 200) {
+				$response = json_decode($response, true);
+				error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+				if($http_code == 401)
+					throw new Exception('Invalid access token provided');
+				return false;
+				
+			} else {
+				$response = json_decode($response, true);
+				if(isset($response['description']))
+					error_log("Request was successfull: {$response['description']}\n");
+				$response = $response['result'];
+			}
+		 
+			return $response;
 			
 		} else {
-			$response = json_decode($response, true);
-			if(isset($response['description']))
-				error_log("Request was successfull: {$response['description']}\n");
-			$response = $response['result'];
+			header("Content-Type: application/json");
+			echo json_encode($parameters);
+			return true;
 		}
-	 
-		return $response;
 	}
 }
