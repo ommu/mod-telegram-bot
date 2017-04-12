3b1ac0b83cc1a842a805b549c2a6990db4f152c1
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-12 23:48:26 +0700

    modify datepicker filter in model

--- modules/telegrambot/models/TelegrambotUserHistory.php
+++ modules/telegrambot/models/TelegrambotUserHistory.php
@@ -229,8 +229,8 @@ class TelegrambotUserHistory extends CActiveRecord
 				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'status_date',
-					'language' => 'ja',
-					'i18nScriptFile' => 'jquery.ui.datepicker-en.js',
+					'language' => 'en',
+					'i18nScriptFile' => 'jquery-ui-i18n.min.js'
 					//'mode'=>'datetime',
 					'htmlOptions' => array(
 						'id' => 'status_date_filter',
