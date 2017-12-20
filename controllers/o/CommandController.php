c11c9bc32261b5625180736ecfeb4b2f275bc7bd
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-12-20 14:38:36 +0700

    add Utility applyViewPath in all controlller (module with composer)

--- controllers/o/CommandController.php
+++ controllers/o/CommandController.php
@@ -48,8 +48,8 @@ class CommandController extends Controller
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
-			} else
-				throw new CHttpException(404, Yii::t('phrase', 'The requested page does not exist.'));
+				Utility::applyViewPath(__dir__);
+			}
 		} else
 			$this->redirect(Yii::app()->createUrl('site/login'));
 	}
--- controllers/o/UserhistoryController.php
+++ controllers/o/UserhistoryController.php
@@ -43,8 +43,8 @@ class UserhistoryController extends Controller
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
-			} else
-				throw new CHttpException(404, Yii::t('phrase', 'The requested page does not exist.'));
+				Utility::applyViewPath(__dir__);
+			}
 		} else
 			$this->redirect(Yii::app()->createUrl('site/login'));
 	}
