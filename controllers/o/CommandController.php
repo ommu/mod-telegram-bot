79c1dfafbb857db0dca2060c37c1582c4db4e8bf
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-02-01 06:13:19 +0700

    modify controller init

--- controllers/o/CommandController.php
+++ controllers/o/CommandController.php
@@ -47,7 +47,6 @@ class CommandController extends Controller
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
-				Utility::applyViewPath(__dir__);
 			}
 		} else
 			$this->redirect(Yii::app()->createUrl('site/login'));
