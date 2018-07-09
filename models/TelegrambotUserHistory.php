039da9c33625e56ae0490d73c9ea7af763becfa9
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-09 12:31:30 +0700

    change filterDatepicker in trait

--- models/TelegrambotUserHistory.php
+++ models/TelegrambotUserHistory.php
@@ -220,25 +220,7 @@ class TelegrambotUserHistory extends CActiveRecord
 				'htmlOptions' => array(
 					//'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('application.libraries.core.components.system.CJuiDatePicker', array(
-					'model'=>$this,
-					'attribute'=>'status_date',
-					'language' => 'en',
-					'i18nScriptFile' => 'jquery-ui-i18n.min.js',
-					//'mode'=>'datetime',
-					'htmlOptions' => array(
-						'id' => 'status_date_filter',
-					),
-					'options'=>array(
-						'showOn' => 'focus',
-						'dateFormat' => 'yy-mm-dd',
-						'showOtherMonths' => true,
-						'selectOtherMonths' => true,
-						'changeMonth' => true,
-						'changeYear' => true,
-						'showButtonPanel' => true,
-					),
-				), true),
+				'filter' => $this->filterDatepicker($this, 'status_date'),
 			);
 			$this->defaultColumns[] = array(
 				'name' => 'status',
